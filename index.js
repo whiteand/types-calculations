e1bfcba702118b86c85af12c067f7d006c304cff
Андрій Білецький < >
2018-08-19 00:18:06 +0300

    Added js realization for types and `getType` function that returns type, and add concat and concatAll functions that merges 2+ types into one(ex. if one variable has different types)

---
+++ index.js
@@ -0,0 +1,335 @@
+const R = require('ramda')
+
+const ONE_OF='ONE_OF'
+const E = 'Empty'
+const S = 1
+const A = 2
+const O = 3
+const P = 4
+
+// is(E|S|A|O|P) :: T -> Boolean
+const isOneOf = t => t === ONE_OF
+const isNotOneOf = t => t !== ONE_OF
+const isNotEmptyType = t => t !== E
+
+const isE = R.allPass([
+  t => t === E,
+  isNotOneOf
+])
+const isS = R.allPass([
+  R.pipe(
+    t => typeof t,
+    R.equals('string')
+  ),
+  t => t !== E,
+  isNotOneOf,
+  isNotEmptyType
+])
+
+const isA = R.allPass([
+  Array.isArray,
+  R.anyPass([R.isEmpty, R.pipe(R.head, isNotOneOf)]),
+  isNotOneOf,
+  isNotEmptyType
+])
+const isO = R.allPass([
+  t => typeof t === 'object',
+  R.complement(Array.isArray),
+  isNotOneOf,
+  isNotEmptyType
+])
+const isP = R.allPass([
+  Array.isArray,
+  R.pipe(R.length, optionsAmount => optionsAmount >= 3),
+  R.pipe(R.head, isOneOf),
+  isNotEmptyType
+])
+
+
+// SETOID ---------------------------------------------------------------------
+
+const containsSameValues = R.compose(R.isEmpty, R.symmetricDifference)
+const containsSameValuesWith = R.compose(R.isEmpty, R.symmetricDifferenceWith)
+
+// equalsS :: S -> S -> Boolean
+const equalsS = R.equals
+
+// equalsA :: A -> A -> Boolean
+const equalsA = (a1, a2) => containsSameValuesWith(equals, a1, a2)
+
+// equalsP :: P -> P -> Boolean
+const equalsP = R.curry((p1, p2) => {
+  const variants1 = p1.slice(1)
+  const variants2 = p2.slice(1)
+  return containsSameValuesWith(equals, variants1, variants2)
+})
+
+// equalsO :: O -> O -> Boolean
+const equalsO = R.curry((obj1, obj2) => {
+  const keys1 = Object.keys(obj1)
+  const keys2 = Object.keys(obj2)
+  const hasSameKeys = containsSameValues(keys1, keys2)
+  const hasSameTypesOfKeys = keys1.every(key => equals(obj1[key], obj2[key]))
+  return hasSameKeys && hasSameTypesOfKeys
+})
+
+const equalsE = R.always(true)
+
+var equals = R.curry((t1, t2) => {
+  if (isS(t1) && isS(t2)) return equalsS(t1, t2)
+  if (isA(t1) && isA(t2)) return equalsA(t1,t2)
+  if (isP(t1) && isP(t2)) return equalsP(t1,t2)
+  if (isO(t1) && isO(t2)) return equalsO(t1,t2)
+  if (isE(t1) && isE(t2)) return equalsE(t1,t2)
+  return false
+})
+
+// ORD ------------------------------------------------------------------------
+
+// getSortedArrayOfTypes :: [T] -> [T]
+const getSortedArrayOfTypes = R.pipe(
+  R.clone,
+  R.sort((t1,t2)=>lte(t1,t2) ? -1 : 1)
+)
+
+// lteSortedTypesArray [T] -> [T] -> Boolean
+const lteSortedTypesArray = R.curry((a1, a2) => {
+  if (R.isEmpty(a1) && R.isEmpty(a2)) return true
+  
+  for (let i = 0; i < a1.length; i++) {
+    const first = a1[i]
+    const second = a2[i]
+    if (equals(first, second)) {
+      continue
+    }
+    if (lte(first, second)) {
+      return true
+    }
+    if (lte(second, first)) {
+      return false
+    }
+  }
+  return true
+})
+
+// lteE :: E -> E -> Boolean
+const lteE = R.always(true)
+
+// lteS :: S -> S -> Boolean
+const lteS  = R.curry((t1, t2) => t1 <= t2)
+
+// lteA :: A -> A -> Boolean
+const lteA = R.curry((a1, a2) => {
+  if (a1.length < a2.length) return true
+  if (a1.length > a2.length) return false
+  const sorted1 = getSortedArrayOfTypes(a1)
+  const sorted2 = getSortedArrayOfTypes(a2)
+  return lteSortedTypesArray(sorted1, sorted2)
+})
+
+// lteP :: P -> P -> Boolean
+const lteP = R.curry((p1, p2) => {
+  if (p1.length < p2.length) return true
+  if (p1.length > p2.length) return false
+  const types1 = p1.slice(1)
+  const types2 = p2.slice(1)
+  const sortedTypes1 = getSortedArrayOfTypes(types1)
+  const sortedTypes2 = getSortedArrayOfTypes(types2)
+  return lteSortedTypesArray(sortedTypes1, sortedTypes2)
+})
+
+// lteO :: O -> O -> Boolean
+const lteO = R.curry((obj1, obj2) => {
+  const keys1 = Object.keys(obj1)
+  const keys2 = Object.keys(obj2)
+  if (keys1.length < keys2.length) return true
+  if (keys1.length > keys2.length) return false
+  const sKeys1 = keys1.sort()
+  const sKeys2 = keys2.sort()
+  const joinedKeys1 = sKeys1.join('')
+  const joinedKeys2 = sKeys2.join('')
+  if (joinedKeys1 < joinedKeys2) return true
+  if (joinedKeys1 > joinedKeys2) return false
+  const types1 = sKeys1.map(k => obj1[k])
+  const types2 = sKeys2.map(k => obj2[k])
+  return lteSortedTypesArray(types1, types2)
+})
+
+// lte :: T -> T -> Boolean
+var lte = R.curry((t1, t2) => {
+  switch (true) {
+    case equals(t1, t2): return true
+    case isE(t1) && isE(t2): return lteE(t1, t2)
+    case isS(t1) && isS(t2): return lteS(t1, t2)
+    case isA(t1) && isA(t2): return lteA(t1, t2)
+    case isO(t1) && isO(t2): return lteO(t1, t2)
+    case isP(t1) && isP(t2): return lteP(t1, t2)
+    case isE(t1): return true
+    case isS(t1): return [isA, isO, isP].some(f => f(t2))
+    case isA(t1): return [isO, isP].some(f => f(t2))
+    case isO(t1): return isP(t2)
+    case isP(t1): return false
+  }
+  return false
+})
+
+// MONOID ---------------------------------------------------------------------
+
+const empty = () => E
+
+// concatS :: S -> S -> P
+const concatS = R.curry((s1, s2) => [ONE_OF, s1, s2])
+
+// concatA :: A -> A -> P
+const concatA = R.curry((a1, a2) => [ONE_OF, a1, a2])
+
+// concatSameStructureO :: O -> O -> O
+const concatSameStructureO = R.curry((obj1, obj2) => {
+  const keys = Object.keys(obj1)
+  const mergeReducer = (resObj, key) => {
+    const t1 = obj1[key]
+    const t2 = obj2[key]
+    const newType = concat(t1,t2)
+    return R.assoc(key, newType, resObj)
+  }
+  return keys.reduce(mergeReducer, {})
+})
+
+// hasSameStructureO :: O -> O -> Boolean
+const hasSameStructureO = R.curry((obj1, obj2) => {
+  const keys1 = Object.keys(obj1)
+  const keys2 = Object.keys(obj2)
+  return containsSameValues(keys1, keys2)
+})
+
+
+// concatS :: S -> S -> T
+const concatO = R.curry((obj1, obj2) => {
+  return hasSameStructureO(obj1, obj2)
+    ? concatSameStructureO(obj1, obj2)
+    : [ONE_OF, obj1, obj2]
+})
+
+const minimifyArrayType = arr => {
+  const sTypes = R.filter(isS, arr)
+  const aTypes = R.filter(isA, arr)
+  const oTypes = R.filter(isO, arr)
+  const pTypes = R.filter(isP, arr)
+  const objTypesReducer = (resOTypes, obj) => {
+      const ind = resOTypes.findIndex(t => hasSameStructureO(t, obj))
+      if (ind < 0) return [...resOTypes, obj]
+      const objToBeMergedLens = R.lensIndex(ind)
+      return R.over(objToBeMergedLens, concatSameStructureO(obj), resOTypes)
+    }
+
+  const concatenatedOTypes = oTypes.reduce(objTypesReducer, [])
+  const resTypes = [...sTypes, ...aTypes, ...concatenatedOTypes, ...pTypes]
+  return resTypes
+}
+
+// concatDifferent :: T -> T -> T
+const concatDifferent = R.curry((t1, t2) => {
+  const types1 = isP(t1) ? t1.slice(1) : [t1]
+  const types2 = isP(t2) ? t2.slice(1) : [t2]
+  const allTypes = R.uniqWith(equals, [...types1, ...types2])
+  const resTypes = minimifyArrayType(allTypes)
+  switch (resTypes.length) {
+    case 0: return empty()
+    case 1: return resTypes[0]
+    default: return [ONE_OF, ...resTypes]
+  }
+})
+
+// concatP :: P -> P -> T
+const concatP = concatDifferent
+
+// concat :: T -> T -> T
+var concat = R.curry((t1, t2) => {
+  switch (true) {
+   case isE(t1): return t2
+   case isE(t2): return t1
+   case equals(t1,t2): return t1
+   case isS(t1) && isS(t2): return concatS(t1,t2)
+   case isA(t1) && isA(t2): return concatA(t1,t2)
+   case isO(t1) && isO(t2): return concatO(t1,t2)
+   case isP(t1) && isP(t2): return concatP(t1,t2)
+  }
+
+  return concatDifferent(t1, t2)
+})
+
+var concatAll = R.reduce((res, t) => concat(res, t), E)
+
+
+const getType = (value) => {
+  const type = R.type(value)
+  switch (true) {
+    case R.contains(type, ['Number', 'Boolean', 'String', 'Null', 'RegExp', 'Undefined']):
+      return type
+    case value instanceof Date:
+      return `Date`
+    case value instanceof Promise:
+      return 'Promise'
+    case type === 'Function':
+      return value.length
+      ? `Function (${value.length})`
+      : `Function (void)`
+    case type === 'Array':
+      return value.length 
+       ? minimifyArrayType(R.pipe(
+          R.map(getType),
+          R.uniqWith(equals))(value))
+       : []
+    case type === 'Object':
+      const keys = Object.keys(value)
+      return keys.reduce((res, key)=>{
+        return R.assoc(key, getType(value[key]), res)
+      }, {})
+    default:
+      return type
+  }
+}
+
+module.exports = {
+  ONE_OF,
+  E,
+  S,
+  A,
+  O,
+  P,
+  isOneOf,
+  isNotOneOf,
+  isE,
+  isS,
+  isA,
+  isO,
+  isP,
+  containsSameValues,
+  containsSameValuesWith,
+  equalsS,
+  equalsA,
+  equalsP,
+  equalsO,
+  equalsE,
+  equals,
+  getSortedArrayOfTypes,
+  lteSortedTypesArray,
+  lteE,
+  lteS,
+  lteA,
+  lteP,
+  lteO,
+  lte,
+  empty,
+  concatS,
+  concatA,
+  concatSameStructureO,
+  hasSameStructureO,
+  concatO,
+  minimifyArrayType,
+  concatDifferent,
+  concatP,
+  concat,
+  concatAll,
+  getType
+}
